d7489ad733b0b663c32a2c2efc749503a72c57e0
andlicon <andresjzabalac@hotmail.com>
2023-09-11 16:21:26 -0400

    ADD: search bar is a controlled component

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,16 @@
 import Raact from 'react';
+import { useSearch } from '../hooks/useSearch';
 
 const SearchBar = () => {
+  const {
+    search,
+    handleSearch,
+    isEmpty } = useSearch();
+
   return (
     <div className='searchBar'>
-      <input type="text" placeholder='Buscador' className='searchBar__input rounded'/>
-      <i className="bi bi-search searchBar__icon"></i>
+      <input type="text" placeholder='Buscador' value={search} onChange={handleSearch} className='searchBar__input rounded' />
+      <i className={`bi bi-search searchBar__icon ${isEmpty ? 'show' : 'hide'}`}></i>
     </div>
   )
 }
