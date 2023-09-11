856c72c4ebb26c200b3d7392256301ac2038a8c3
andlicon <andresjzabalac@hotmail.com>
2023-09-11 17:05:41 -0400

    ADD: banner component

---
+++ components/Banner.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Banner = () => {
+  return (
+    <div className='banner'>
+      Este es el banner
+    </div>
+  )
+}
+export default Banner;
---
+++ hooks/useBanner.ts
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+const useBanner = () => {
+  const hi = 'hi';
+
+  return({
+      hi
+    }
+  )
+}
+export default useBanner;
