eef53299da347b681f9d147b2a8780169a44cfb4
andlicon <andresjzabalac@hotmail.com>
2023-09-11 17:25:17 -0400

    ADD: banner size

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Banner = ({ image }) => {
   return (
     <div className='banner'>
-      <img src={image.url.src} alt={image.alt} />
+      <img
+        src={image.url.src}
+        alt={image.alt}
+        className='banner_img mx-auto' />
     </div>
   )
 }
