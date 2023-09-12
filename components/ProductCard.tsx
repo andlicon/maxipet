26badbd1993dbf07c2dcb38d82529c4d5d17f2b6
andlicon <andresjzabalac@hotmail.com>
2023-09-11 22:16:46 -0400

    ADD: Product Card

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
+const rateArray = [1, 2, 3, 4, 5];
+
 const ProductCard = ({ product }) => {
   return (
     <div className='productCard'>
       <div>
         <img src={product.image} alt="" className='productCard__img' />
       </div>
-      <div className='productCard__info mt-4'>
+      <div className='productCard__info mt-4 me-2'>
         <p className='productCard__name'>{product.name}</p>
-        <p>{product.rate}</p>
+        {
+          rateArray.map((number) => {
+            const isChecked = product.rate >= number;
+
+            return (
+              <span className={`rate${isChecked ? ' checked' : ''}`}>
+              </span>
+            )
+          })
+        }
         <p className='productCard__price'>{product.price}</p>
       </div>
     </div>
