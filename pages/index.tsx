41129ea088d25a13cca7ebfe62c3a23ec8b4069b
andlicon <andresjzabalac@hotmail.com>
2023-09-12 10:58:20 -0400

    ADD: news product section modularized

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,14 @@ import Image from 'next/image'
 import Header from '../components/NavBar';
 import BannerDisplay from '../components/BannerDisplay';
 import Section from '../components/Section';
-import ProductCard from '../components/ProductCard';
-import AllServices from '../components/AllServices';
 import Brand from '../components/Brand'
 import SectionCategory from '../components/SectionCategory';
 import SectionFeatured from '../components/SectionFeatured';
 import SectionPopular from '../components/SectionPopular';
-import {
-  newList
-} from '../constant/products';
+import SectionNews from '../components/SectionNews';
+import AllServices from '../components/AllServices';
 import {
   bannerImages,
-  newAd,
   brandAdditional
 } from '../constant/images';
 import {
@@ -32,36 +28,17 @@ function HomePage() {
       <div className="container page__content mx-auto">
         <SectionCategory />
         <SectionFeatured />
-
         {/* add */}
         <div className='flex ad w-full'>
           <Image src={ad2} alt="A dog eating its dog food" className='w-1/2' />
           <Image src={ad1} alt="A shovel with dog food" className='w-1/2' />
         </div>
-
         <SectionPopular />
-
         {/* add */}
         <div className='ad w-full'>
           <Image src={ad3} alt="A happy dog lay down over the grass, playing with its toy" className='w-full' />
         </div>
-
-        {/* News products */}
-        <Section
-          title='Últimos productos'
-          subTitle='Lorem ipsum dolor sit amet.'
-          additional={newAd}
-          childrenDisplay='grid'>
-          {
-            newList.map((product) => {
-              return (
-                <ProductCard product={product} key={product.id} />
-              )
-            })
-          }
-        </Section>
-
-        {/* Services */}
+        <SectionNews />
         <AllServices />
 
         {/* News products */}
