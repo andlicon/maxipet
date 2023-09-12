a5f4b908808959f98e752bf9143c0933b0a6fc9d
andlicon <andresjzabalac@hotmail.com>
2023-09-11 23:11:03 -0400

    ADD: section image moves sides

--- components/Section.tsx
+++ components/Section.tsx
@@ -9,7 +9,7 @@ const Section = ({ children, title, subTitle, image }) => {
         <div className='section__content flex mt-6'>
           {
             image != null
-              ? <img src={image.image} alt="" className='me-5' />
+              ? <img src={image.image} className={image.side ? 'order-1 ms-5' : 'me-5'} alt="" />
               : null
           }
           <div className='flex justify-between w-full flex-wrap'>
