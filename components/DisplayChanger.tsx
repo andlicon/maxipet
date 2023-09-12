02800fb0c6aec07f34995c868576064013ee45b1
andlicon <andresjzabalac@hotmail.com>
2023-09-12 13:32:20 -0400

    ADD: fixing warning

--- components/DisplayChanger.tsx
+++ components/DisplayChanger.tsx
@@ -5,8 +5,6 @@ const DisplayChanger = ({ current, changeImage }) => (
     type="button"
     className={`display-changer rounded ${current ? 'current' : ''}`}
     onClick={changeImage}
-  >
-
-  </button>
+  />
 );
 export default DisplayChanger;
